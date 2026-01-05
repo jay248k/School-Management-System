f2edc7ce8b42c4638609dae4ff874b654904db64
jayparmar2787@gmail.com <jayparmar2787@gmail.com>
2026-01-05 19:47:24 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,17 @@ import express from 'express';
 import './utils/db.js'
 import StudentRouter from './Router/Student.Route.js';
 import FeesRouter from './Router/Fees.Route.js';
+import AdminRouter from './Router/Admin.Route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
+app.use(cors());
 app.use("/api/Student",StudentRouter);
 app.use("/api/fees",FeesRouter);
+app.use("/api/admin",AdminRouter);
 
 const port=5000;
 
