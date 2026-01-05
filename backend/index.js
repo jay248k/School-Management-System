ea1aefceb148505f2699977dd67e317158a1207b
prajapati bansi <prajapatibansi1510@gmail.com>
2026-01-05 10:00:15 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import StudentRouter from './Router/Student.Route.js';
 import FeesRouter from './Router/Fees.Route.js';
 import AdminRouter from './Router/Admin.Route.js';
 import cookieParser from 'cookie-parser';
-<<<<<<< HEAD
 import cors from "cors";
 const app = express();
 app.use(cors());
@@ -16,15 +15,6 @@ app.use("/api/fees", FeesRouter);
 app.use("/api/admin", AdminRouter);
 
 const port = 5000;
-=======
-const app=express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use("/api/Student",StudentRouter);
-app.use("/api/fees",FeesRouter);
-app.use("/api/admin",AdminRouter);
->>>>>>> f66a65e47a67249e77334d60ed7bde430a8b785c
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
