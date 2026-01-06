166ebfba946e057e117a453c4240ba9e9e8f5644
prajapati bansi <prajapatibansi1510@gmail.com>
2026-01-06 21:02:07 +0530

    admin login sucessfully

--- principal-teacher/src/main.jsx
+++ principal-teacher/src/main.jsx
@@ -4,6 +4,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -16,8 +19,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         draggable
         theme="light"
       />
-
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </>
   </React.StrictMode>
 );
--- principal-teacher/src/services/admin.api.js
+++ principal-teacher/src/services/admin.api.js
@@ -1,6 +1,26 @@
 import axios from "axios";
-
+import { toast } from "react-toastify";
 
 const API = axios.create({
-  baseURL: "http://loaclhost:5000/"
-})
+  baseURL: `${import.meta.env.VITE_API_URL}/api/admin`,
+});
+export const adminLoginAPI = async (payload) => {
+  console.log(payload);
+  try {
+    const res = await API.post("/login", payload);
+
+    if (res.data.success) {
+      return true;
+    }
+    toast.error(res.data.message || "Invalid credentials");
+    return false;
+  } catch (error) {
+    const message =
+      error?.response?.data?.message ||
+      error?.message ||
+      "Login failed";
+
+    toast.error(message);
+    return false;
+  }
+};
