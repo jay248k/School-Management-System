135f6ad6aeb4506474bbdf3ede6d11b2df70f8ed
prajapati bansi <prajapatibansi1510@gmail.com>
2026-01-07 21:23:10 +0530

    autheticatation added in backend

--- backend/Router/Teacher.Route.js
+++ backend/Router/Teacher.Route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { TeacherDelete, TeacherLogin, TeacherRegistration, TeacherUpdate } from '../Controller/Teacher.Controller.js';
+import isAuthenticated from '../middleware/isAutheticated.js';
+import isAdmin from '../middleware/isAdmin.js';
 
-const TeacherRouter=express.Router();
+const TeacherRouter = express.Router();
 
-TeacherRouter.post('/register',TeacherRegistration)
-TeacherRouter.post('/login',TeacherLogin)
-TeacherRouter.put('/:id/update',TeacherUpdate)
-TeacherRouter.delete('/:id/delete',TeacherDelete)
+TeacherRouter.post('/register', isAuthenticated, isAdmin, TeacherRegistration)
+TeacherRouter.post('/login', TeacherLogin)
+TeacherRouter.put('/:id/update', isAuthenticated, isAdmin, TeacherUpdate)
+TeacherRouter.delete('/:id/delete', isAuthenticated, isAdmin, TeacherDelete)
 
 export default TeacherRouter;
