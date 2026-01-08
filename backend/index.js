f5522797b832146a71859bc91a6ac0fcc0f65f21
jayparmar2787@gmail.com <jayparmar2787@gmail.com>
2026-01-08 05:36:40 +0530

    Testing error

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,26 @@ import AdminRouter from './Router/Admin.Route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import TeacherRouter from './Router/Teacher.Route.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use("/api/Student",StudentRouter);
 app.use("/api/fees",FeesRouter);
 app.use("/api/admin",AdminRouter);
 app.use("/api/teacher",TeacherRouter);
-
-const port=5000;
+app.use((req, res) => {
+  res.status(404).json({ message: "API not found" });
+});
+const port=process.env.PORT||5000;
 
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`);
